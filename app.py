74cd4de989353b9eb9baebad15550e5cc6dc9868
Seungjae Ryan Lee <seungjaeryanlee@gmail.com>
2021-01-03 12:39:42 +0000

    Setup database repo for backend

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ CORS(app)
 @app.route('/api/all/<date>')
 def get_all_data(date):
     year, month, day = date.split("-")
-    filepath = f"../../self-management/data/raw/{year}/{month}/{year}-{month}-{day}.txt"
+    filepath = f"ttrack-database/log/{year}/{month}/{year}-{month}-{day}.txt"
     raw_lines = parser.get_raw_lines(filepath)
     duration_and_lines = parser.get_duration_and_lines(raw_lines)
     info_dict = parser.get_info_dict(duration_and_lines)
