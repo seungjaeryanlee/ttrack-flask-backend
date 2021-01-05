77f1858030dd79e11718e65cfeb02f7e9130a96f
Seungjae Ryan Lee <seungjaeryanlee@gmail.com>
2021-01-05 15:55:03 +0000

    Expose API to update classifier rules

--- app.py
+++ app.py
@@ -6,6 +6,15 @@ from flask_cors import CORS
 import parser
 
 
+LABEL_TO_DB_FILES = {
+    "School and Work": "school_and_work_tasks.txt",
+    "Personal Development": "personal_development_tasks.txt",
+    "Personal Well-being": "personal_well_being_tasks.txt",
+    "Misc": "misc_tasks.txt",
+    "Personal Enjoyment": "personal_enjoyment_tasks.txt",
+    "Ignore": "ignore_tasks.txt",
+}
+
 app = Flask(__name__)
 # TODO: Probably not needed when actually deployed?
 CORS(app) 
@@ -35,20 +44,12 @@ def get_log(date):
 
 @app.route('/api/log/save', methods=['PUT'])
 def save_log():
+    # TODO: Implement
     return { "status": "success" }
 
 
 @app.route('/api/rules/all')
 def get_all_rules():
-    LABEL_TO_DB_FILES = {
-        "School and Work": "school_and_work_tasks.txt",
-        "Personal Development": "personal_development_tasks.txt",
-        "Personal Well-being": "personal_well_being_tasks.txt",
-        "Misc": "misc_tasks.txt",
-        "Personal Enjoyment": "personal_enjoyment_tasks.txt",
-        "Ignore": "ignore_tasks.txt",
-    }
-
     task_to_label = {}
     for label, db_filename in LABEL_TO_DB_FILES.items():
         with open('ttrack-database/parser_rules/{}'.format(db_filename), 'r') as f:
@@ -59,3 +60,15 @@ def get_all_rules():
                 task_to_label[task] = label
 
     return task_to_label
+
+
+@app.route('/api/rules/add', methods=['POST'])
+def add_rule():
+    task = request.json["task"]
+    label = request.json["task_label"]
+    db_filename = LABEL_TO_DB_FILES[label]
+
+    with open('ttrack-database/parser_rules/{}'.format(db_filename), 'a') as f:
+        f.write(f"{task}\n")
+
+    return { "status": "success" }
